6a9ab4dc1a1ad3d0ddcaa3907cdf09d43e83b4de
TomOwen <tho@cox.net>
2012-04-23 09:24:38 -0700

    deleted commented code from perform operation

--- Calculator/CalculatorBrain.m
+++ Calculator/CalculatorBrain.m
@@ -89,34 +89,4 @@
     }
     return [self popOperandOffStack:stack];
 }
-/*
-    double result = 0;
-    //NSLog(@"operation = %@",operation);
-    if  ([operation isEqualToString:@"+"]) {
-        result = [self popOperand] + [self popOperand];
-    } else if ([operation isEqualToString:@"*"]) {
-        result = [self popOperand] * [self popOperand];
-    } else if ([operation isEqualToString:@"-"]) {
-        double pop1 = [self popOperand];
-        double pop2 = [self popOperand];
-        result = pop2 - pop1;
-    } else if ([operation isEqualToString:@"/"]) {
-        double operand1 = [self popOperand];
-        double operand2 = [self popOperand];
-        if (operand1) {
-        result = operand2/operand1;
-        }
-    } else if ([operation isEqualToString:@"π"]) {
-        result = 3.14159;
-    } else if ([operation isEqualToString:@"sin"]) {
-        result = sin([self popOperand]);
-    } else if ([operation isEqualToString:@"cos"]) {
-        result = cos([self popOperand]);
-    } else if ([operation isEqualToString:@"sqrt"]) {
-        result = sqrt([self popOperand]);
-    }
-    [self pushOperand:result];
-    return result;
-}
-*/
 @end
